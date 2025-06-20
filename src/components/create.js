fcf9745a537341ace03d9cb068162d62389d4e28
NA\pateldat <dattram.patel@colliers.com>
2025-06-19 21:14:57 -0400

    .

--- src/components/create.js
+++ src/components/create.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import QRCode from 'react-qr-code';
 import CryptoJS from 'crypto-js';
 
--- src/components/scan.js
+++ src/components/scan.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import QRCode from 'react-qr-code';
+import { useState } from 'react';
 import BarcodeScanner from "./BarcodeScanner";
 import CryptoJS from 'crypto-js';
 
